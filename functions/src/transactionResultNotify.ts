5e8df81f975280f1401b25dcf3bd9d187d73049e
Carlos <cgumuciolabbe@gmail.com>
2024-08-17 16:12:08 +0800

    feat: payment, notify email notif stable

--- functions/src/transactionResultNotify.ts
+++ functions/src/transactionResultNotify.ts
@@ -1,21 +1,21 @@
 import { onRequest } from 'firebase-functions/v2/https';
 import * as logger from 'firebase-functions/logger';
-import { getAuth, getFirestore } from './index';
+import { getFirestore, token } from './index';
 import { defaultEmailSender, paymentSettings } from './config';
 import axios from 'axios';
 
 const sendEmailUrl = process.env.SEND_EMAIL_URL;
 
 export const transactionResultNotify = onRequest(
-  { region: 'southamerica-west1', memory: '128MiB', maxInstances: 1 },
+  { cors: true, region: 'southamerica-west1', memory: '128MiB', maxInstances: 1 },
   async ({ body, query, method }, res) => {
-      if (method === 'OPTIONS') {
-        logger.info('options running');
-        res.send(200);
-        return;
-      }
-    logger.info('beggining transaction result');
+    if (method === 'OPTIONS') {
+      logger.info('options running');
+      res.send(200);
+      return;
+    }
 
+    logger.info('beggining transaction result');
     const { status } = body;
     const { appointmentId } = query;
     if (!status) {
@@ -23,47 +23,35 @@ export const transactionResultNotify = onRequest(
       res.status(400).send('Missing status in request body');
       return;
     }
+    // validating if request has appointmentId
     if (!appointmentId) {
       logger.error('there was no appointmentId within the query', query);
       res.status(400).send('Missing document ID in request');
       return;
     }
+
+    let appointmentInfo: FirebaseFirestore.DocumentData | undefined;
     const db = getFirestore();
-    const auth = getAuth();
-    const docRef = db.collection('appointments').doc(String(appointmentId));
     try {
+      const docRef = db.collection('appointments').doc(String(appointmentId));
       logger.info('retrieving appointment document with id:', appointmentId);
       const docSnapshot = await docRef.get();
-      const appointmentInfo = docSnapshot.data();
-      logger.info('appointment details', appointmentInfo);
-      if (docSnapshot.exists) {
-        if (status === 'success') {
-          await docRef.update({
-            isPaid: 'Pagado',
-          });
-          const token = await auth.getUser(appointmentInfo?.customer?.id);
-          await axios.post(`${sendEmailUrl}/`, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-            data: {
-              providerName: appointmentInfo?.provider.firstname,
-              customerName: appointmentInfo?.customer.firstname,
-              serviceName: appointmentInfo?.servicio.name,
-              scheduledDate: appointmentInfo?.scheduledDate,
-              scheduledTime: appointmentInfo?.scheduledTime,
-              templateName: 'scheduled-appointment.html',
-              options: {
-                from: defaultEmailSender,
-                to: appointmentInfo?.provider?.email,
-                subject: 'Nueva sesión agendada',
-                text: `Hola ${appointmentInfo?.provider?.firstname}, tienes una nueva sesión de ${appointmentInfo?.servicio.name} confirmada para el día ${appointmentInfo?.scheduledDate} a las ${appointmentInfo?.scheduledTime} con ${appointmentInfo?.customer?.firstname} ${appointmentInfo?.customer?.lastname}`,
-              },
-            },
-          });
-          const paymentDate = new Date();
-          await db.collection('payments').add({
-            appointmentId: appointmentId,
+      appointmentInfo = docSnapshot.data();
+      if (!docSnapshot.exists) {
+        throw new Error('Appointment document does not exist');
+      }
+      if (status === 'success') {
+        await docRef.update({
+          isPaid: 'Pagado',
+        });
+        const paymentDate = new Date();
+        const paymentsCollectionRef = db.collection('payments');
+        const paymentQuerySnapshot = await paymentsCollectionRef
+          .where('appointmentId', '==', appointmentId)
+          .get();
+        if (paymentQuerySnapshot.empty) {
+          await paymentsCollectionRef.add({
+            appointmentId,
             status: 'pending',
             paymentDate: new Date(),
             paymentDueDate: new Date(
@@ -71,24 +59,56 @@ export const transactionResultNotify = onRequest(
             ),
             amount: appointmentInfo?.servicio?.price * (1 - paymentSettings.appCommission),
           });
-          res.status(200).send('Appointment updated successfully');
-          return;
-        } else {
-          await docRef.update({
-            isPaid: 'failed',
-          });
-          res.status(422).send('Payment failed');
-          return;
         }
       } else {
-        res
-          .status(404)
-          .send(`Appointment under id: ${appointmentId} either doesnt exist or was deleted`);
+        await docRef.update({
+          isPaid: 'failed',
+        });
+        res.status(422).send('Payment failed');
         return;
       }
     } catch (error) {
       logger.error('error updating appointment', error);
       res.status(500).send('Error updating appointment');
+      return;
+    }
+    try {
+      const rawToken = await token;
+      const emailsCollectionRef = db.collection('emails');
+      const emailSnap = await emailsCollectionRef.where('appointmentId', '==', appointmentId).get();
+      logger.info(emailSnap.docs);
+      if (emailSnap.empty) {
+        logger.info('inside emailSnap empty');
+        const emailRegistered = await emailsCollectionRef.add({
+          appointmentId,
+          sent: true,
+          date: new Date(),
+        });
+        await axios.post(String(sendEmailUrl), {
+          method: 'post',
+          headers: {
+            authorization: `Bearer ${rawToken}`,
+          },
+          providerName: appointmentInfo?.provider.firstname,
+          customerName: appointmentInfo?.customer.firstname,
+          serviceName: appointmentInfo?.servicio.name,
+          scheduledDate: appointmentInfo?.scheduledDate,
+          scheduledTime: appointmentInfo?.scheduledTime,
+          templateName: 'scheduled-appointment.html',
+          options: {
+            from: defaultEmailSender,
+            to: appointmentInfo?.provider?.email,
+            subject: 'Nueva sesión agendada',
+            text: `Hola ${appointmentInfo?.provider?.firstname}, tienes una nueva sesión de ${appointmentInfo?.servicio.name} confirmada para el día ${appointmentInfo?.scheduledDate} a las ${appointmentInfo?.scheduledTime} con ${appointmentInfo?.customer?.firstname} ${appointmentInfo?.customer?.lastname}`,
+          },
+        });
+
+        logger.info('email sent and record created', emailRegistered);
+      }
+      res.status(200).send('Email notification sent successfully');
+    } catch (error) {
+      logger.error('error sending the email notification', error);
+      res.status(500).send('error sending the email notification');
       return;
     }
   },
--- functions/src/validations/unAuthorized.ts
+++ functions/src/validations/unAuthorized.ts
@@ -1,5 +1,7 @@
 import { IncomingHttpHeaders } from "http2";
 import { Response } from 'express';
+import * as logger from 'firebase-functions/logger';
+
 
 /**;
  * unAuthorized - Function to check if the request is authorized
@@ -11,6 +13,7 @@ import { Response } from 'express';
 
 export function unAuthorized(headers: IncomingHttpHeaders, res: Response) {
   const authToken = headers.authorization;
+  logger.info('auth token from unAuthorized helper', authToken)
   if (!authToken) {
     res.status(401).send('Unauthorized');
     return;
