d9fa1983afe436c3b907e587c80b888320ede4de
Carlos <cgumuciolabbe@gmail.com>
2024-08-23 23:42:29 +0800

    fix: redirect flujo registro usuario

--- functions/src/config.ts
+++ functions/src/config.ts
@@ -5,7 +5,7 @@ export const sendEmailUrl = process.env.SEND_EMAIL_URL;
 export const defaultEmailSender = 'Blui.cl <francisco.durney@blui.cl>';
 
 export const paymentSettings = {
-  appCommission: 0.075,
+  appCommission: 0.02,
   currency: 'CLP',
   paymentMethods: 99,
   providerPayAfterDays: 3
