ca033872c7e7193e7b84f2c8fdd78d473ad9fd55
Carlos <cgumuciolabbe@gmail.com>
2024-10-16 13:43:42 +0800

    fix?

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -26,8 +26,6 @@ const firebaseConfig = {
 const env = import.meta.env.VITE_ENV;
 const mode = import.meta.env.MODE;
 
-console.log(env, mode, firebaseConfig);
-
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig, 'blui');
 export const auth = getAuth(app);
@@ -39,7 +37,7 @@ functions.region = 'southamerica-west1';
 // Check if window is defined before initializing Firebase Analytics
 export const analytics = typeof window !== 'undefined' ? getAnalytics(app) : null;
 
-if (import.meta.env.VITE_ENV === 'dev' || import.meta.env.MODE === 'test') {
+if (env === 'dev' || mode === 'test') {
   console.log('connecting to emulators');
   connectAuthEmulator(auth, 'http://localhost:9099/auth');
   connectFirestoreEmulator(db, 'localhost', 8080);
